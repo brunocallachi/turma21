dc17cde569b7fbeae8445c36488d343a360d1ac8
brunocallachi <brunocallachi@hotmail.com>
2021-05-04 13:16:39 -0300

    ok

---
+++ Java/AULAS/src/exercicios/PrimeiroExercicio.java
@@ -0,0 +1,30 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class PrimeiroExercicio {
+	public static void main (String args[])
+		{
+			Scanner leia = new Scanner(System.in);
+			int N; 
+			
+			
+			//tudo é comando escreva
+			System.out.println("Digite um numero: ");
+			N = leia.nextInt();
+			
+			//final da leitura da variavel N
+			
+			if(N > 100)
+			{
+			System.out.println("O numero é: " + N);
+			}
+			
+					
+			
+			if (N < 100)
+			{
+			System.out.println("O numero equivale a : " + (N=0));
+			}
+		}
+}
